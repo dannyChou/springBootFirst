f959407048f2491611c8d9201401e9802775dd76
dannychou <danny75@gmail.com>
2019-12-30 23:13:14 +0800

    Generate

---
+++ src/main/java/com/danny/Firstjava/FirstjavaApplication.java
@@ -0,0 +1,13 @@
+package com.danny.Firstjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danny/Firstjava/FirstjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.Firstjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
